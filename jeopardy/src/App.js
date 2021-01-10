d4b0e400271a11316570f217e273ddaaf0377270
Theveenan <theveenan@gmail.com>
2021-01-10 00:20:02 -0500

    Got fetch categories working

--- jeopardy/src/App.js
+++ jeopardy/src/App.js
@@ -1,27 +1,43 @@
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  const api_url=("http://jservice.io/api/categories?&count=100")
+class App extends Component{
+  constructor(){
+    super();
+    this.state={
+      a:"ayo"
+    };
+
+
+  }
+
+
+  render(){
+//Fetch category data in first promise
+  let api_url=("http://jservice.io/api/categories?&count=100")
   let myPromise= new Promise((resolve,reject)=>{
     fetch(api_url)
         .then(res=>res.json())
-        .then(data=>{
-            resolve(data)
+        .then(categories=>{
+            resolve(categories)
         }    
         )
     })
+//Rest of program nested within .then
+  myPromise.then((categories)=>{
+    console.log(categories)
 
-  let categories=[]
-  myPromise.then((x)=>{
-    categories=x.slice()
     })
 
 
+
+// RETURN STATEMENT JSX
   return (
     <div className="App">
-
+      <p>{this.state.a}</p>
     </div>
   );
-}
 
+  }
+}
 export default App;
